8127c6b845ea4b939926d6ab12f1450d97f5d87c
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-01-12 11:26:56 +0100

    index for hero

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -2,14 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React from "react";
-import { sleep } from "../../utils";
-import Button from "../Button";
 import Modal from "../Modal";
-import { useEffect, useState } from 'react';
-import useWallet from '../../contexts/wallet';
-import ConnectWallet from '../wallet/connectWallet';
-
-
+import { CardanoWallet } from "@meshsdk/react";
 
 
 type LinksType = {
@@ -24,20 +18,7 @@ const NavLinks: LinksType[] = [
 ];
 
 const Navbar = () => {
-  const [open, setOpen] = React.useState<boolean>(false);
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const { connecting, walletNameConnected, connectWallet, walletConnected, connectedAddress, currentNetwork } = useWallet();
-
-  const [wallet, setWallet] = React.useState<boolean>(false);
-
-  const handleConnectWallet = async () => {
-    setLoading(true);
-    await sleep(2000).then(() => {
-      setLoading(false);
-      setWallet(true);
-      setOpen(true);
-    });
-  };
+const [open, setOpen] = React.useState<boolean>(false);
 
 
   const router = useRouter();
@@ -112,46 +93,8 @@ const Navbar = () => {
             </div>
           </ul>
 
-          <div className="dropdown dropdown-hover">
-
-            {
-          connecting ? 
-
-      <Button className="bg-light text-primary hover:bg-light font-semibold hover:border-none">
-        Connecting...
-      </Button>
- : 
-          <label tabIndex={0}>
-          { walletConnected ? (
-      <Button className="bg-light text-primary hover:bg-light font-semibold hover:border-none">
-        $directEd: Connected to {walletNameConnected}
-      </Button>
-    ) : (
-      <ConnectWallet/>
-    )}
-
-
-    
-        </label>
-        }
-            {!walletConnected && (
-              <div
-                tabIndex={0}
-                className="hidden md:block dropdown-content card card-compact w-52 p-2 shadow bg-white mt-1"
-              >
-                <div className="card-body">
-                  <h3 className="text-lg font-semibold">
-                    Start by connecting your wallet
-                  </h3>
-                  <p>
-                    Need help? Follow this{" "}
-                    <Link href={"#"}>
-                      {/* <a className="underline">link</a>  */}
-                    </Link>
-                  </p>
-                </div>
-              </div>
-            )}
+          <div className="bg-light text-primary hover:bg-light font-semibold hover:border-none">
+            <CardanoWallet/>
           </div>
         </div>
       </nav>
