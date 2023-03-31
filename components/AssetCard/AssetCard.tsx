8d6f16775534bfdafec658eb7459b5dd3461a63b
Adetayo <tuneymitch@gmail.com>
2023-03-31 06:46:21 +0100

    feat: donor portal

--- components/AssetCard/AssetCard.tsx
+++ components/AssetCard/AssetCard.tsx
@@ -6,18 +6,14 @@ const AssetCard = ({ assets }: { assets: Assets[] }) => {
 	return (
 		<>
 			{assets.map((asset) => (
-				<div key={asset.id}>
-					{/* image */}
-					<div className='flex justify-center pt-0'>
+				<div key={asset.id} className='assets'>
+					<div>
 						<AssetImage imageSrc={asset.image} imageAlt={asset.name} />
 					</div>
-					{/* title, options and quantity */}
-					<div className='flex flex-col px-4 space-y-1'>
-						<div className='flex flex-col -space-y-0.5'>
-							<h5 className='text-center font-bold'>{asset.name}</h5>
-							<p className='text-center text-[#374756] text-sm'>
-								{asset.caption}...
-							</p>
+					<div>
+						<div>
+							<h5>{asset.name}</h5>
+							<p>{asset.caption}...</p>
 							<Link
 								href={`/legends/${asset.id}`}
 								className='text-[#374756] text-base text-center cursor-pointer'
--- pages/donate/index.tsx
+++ pages/donate/index.tsx
@@ -3,7 +3,7 @@ import { NextPage } from 'next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
 import { FaChevronLeft } from 'react-icons/fa'
-import { Button, Meta, Modal } from '../../components'
+import { Button, Meta, Modal, TierCard } from '../../components'
 import { ModalHandler } from '../../components/Modal/Modal'
 import { amounts, options } from '../../lib/donorAmounts'
 import { GrFormClose } from 'react-icons/gr'
@@ -12,22 +12,20 @@ import { GrFormClose } from 'react-icons/gr'
 
 const Donate: NextPage = () => {
 	const [amount, setAmount] = useState<string>('')
-	const [image, setImage] = useState<number | null>(null)
-
 	const modalRef = useRef<ModalHandler>(null)
 
 	const router = useRouter()
 
-	const handleDonate = () => {
-		if (amount === '₳1000' || amount === '₳2000') {
-			router.push('https://pay.nmkr.io/?p=fbd4da6ef7b14acda66dff5515877a46&c=1')
-		} else {
-			if (amount === '₳40')
-				router.push(
-					'https://pay.nmkr.io/?p=296d3a8e72444590856ccc902b67f9b3&c=1'
-				)
-		}
-	}
+	// const handleDonate = () => {
+	// 	if (amount === '₳1000' || amount === '₳2000') {
+	// 		router.push('https://pay.nmkr.io/?p=fbd4da6ef7b14acda66dff5515877a46&c=1')
+	// 	} else {
+	// 		if (amount === '₳40')
+	// 			router.push(
+	// 				'https://pay.nmkr.io/?p=296d3a8e72444590856ccc902b67f9b3&c=1'
+	// 			)
+	// 	}
+	// }
 
 	const openModal = () => modalRef.current?.openModal()
 
@@ -36,15 +34,6 @@ const Donate: NextPage = () => {
 	return (
 		<>
 			<Meta title='Donate' description='Donate to student' />
-			{/* <div className='donate-banner'>
-				<Image
-					className='donate-banner__image'
-					src='/static/images/banner.png'
-					alt='banner'
-					height={500}
-					width={1500}
-				/>
-			</div> */}
 			<main className='donate container'>
 				<div className='donate__title-section'>
 					<FaChevronLeft
@@ -56,61 +45,54 @@ const Donate: NextPage = () => {
 					<h3>Donate to Kagumo High School</h3>
 				</div>
 				<div className='donate__donations'>
-					<h4>DirectEd Lions Collection</h4>
-
-					<div className='donate__donations-amounts'>
-						{amounts.map((amt, index: number) => (
-							<Button
-								size='small'
-								variant={amt.amount === amount ? 'primary' : ''}
-								onClick={() => {
-									setAmount(amt.amount)
-									setImage(index + 111)
-								}}
-								key={amt.amount}
-								// className={`${amount === amt ? '' : ''}`}
-								noShadow
-							>
-								{amt.title}
-							</Button>
-						))}
-					</div>
-					{amount && (
-						<div className='donate__nft-content'>
-							<Image
-								onClick={openModal}
-								src={`/static/images/${image ? image : 111}.png`}
-								alt='NFT'
-								width={100}
-								height={100}
-								className='donate__nft-image'
-							/>
-
-							<p>Click to see a sample NFT</p>
+					<div className='donate__tier-options'>
+						<div>
+							<h4>DirectEd Lions Collection</h4>
+							<p>Donate and receive a DirectEd Lions NFT</p>
+							<p>Click tiers to learn more</p>
+							<div className='donate__tiers'>
+								{amounts.map((amt, index: number) => (
+									<Button
+										size='small'
+										variant={amt.title === amount ? 'primary' : ''}
+										onClick={() => setAmount(amt.title)}
+										key={amt.amount}
+										noShadow
+									>
+										{amt.title}
+									</Button>
+								))}
+							</div>
 						</div>
-					)}
-
-					<div className='donate__donations-amounts'>
-						{options.map((option) => (
-							<Button size='small' noShadow>
-								{option.title}
-							</Button>
-						))}
+						<div>
+							<h4>Direct Donation</h4>
+							<p>No DirectEd Lions Collection NFT</p>
+							<div className='donate__tiers'>
+								{options.map((option) => (
+									<Button
+										size='small'
+										noShadow
+										variant={option.title === amount ? 'primary' : ''}
+										onClick={() => setAmount(option.title)}
+									>
+										{option.title}
+									</Button>
+								))}
+							</div>
+						</div>
 					</div>
-					<Button onClick={handleDonate} variant='primary'>
-						Donate Now
-					</Button>
+					{amount && <TierCard onClick={() => setAmount('')} />}
 				</div>
 			</main>
 			<Modal ref={modalRef}>
 				<div className='donate__preview'>
-					<Image
+					{/* <Image
 						src={`/static/images/${image ? image : 111}.png`}
 						alt='NFT'
 						height={350}
 						width={350}
 						className='donate__preview-image'
-					/>
+					/> */}
 					<GrFormClose
 						size={25}
 						color='white'
