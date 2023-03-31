11e21f0a0bc30a68b1a563a25031c57282175e67
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-03-31 08:28:59 +0100

    fix: gallery page

--- pages/legends/[id].tsx
+++ pages/legends/[id].tsx
@@ -1,41 +1,44 @@
-import React from 'react'
-import { useRouter } from 'next/router'
-import { data } from '../../data/assets'
-import Image from 'next/image'
-import { Meta } from '../../components'
+import React from "react";
+import { useRouter } from "next/router";
+import { data } from "../../data/assets";
+import Image from "next/image";
+import { Meta } from "../../components";
+import { BackIcon } from "../../icons";
 
 const Legend = () => {
-	const router = useRouter()
-	const pageId = Number(router.query.id)
+  const router = useRouter();
+  const pageId = Number(router.query.id);
 
-	const singleAsset = data.filter((asset) => asset.id === pageId)
+  const singleAsset = data.filter((asset) => asset.id === pageId);
 
-	return (
-		<>
-			{singleAsset.map((asset) => (
-				<>
-					<Meta title={asset.name} description={asset.caption} />
-					<div
-						className='w-max h-32 md:h-48 mx-auto mt-12 mb-80'
-						key={asset.id}
-					>
-						<Image
-							src={asset.image}
-							alt={asset.name}
-							height={200}
-							width={500}
-						/>
-					</div>
-					<div>
-						<h1 className='text-5xl mt-12 text-center'>{asset.name}</h1>
-						<div className='w-3/4 mx-auto my-16'>
-							<p className='text-center'>{asset.description}</p>
-						</div>
-					</div>
-				</>
-			))}
-		</>
-	)
-}
+  return (
+    <>
+      {singleAsset.map((asset) => (
+        <>
+          <Meta title={asset.name} description={asset.caption} />
 
-export default Legend
+          <div className="legend" key={asset.id}>
+            <div className="legend__heading">
+              <button>
+                <BackIcon height={24} width={24} 
+                  onClick={() => router.back()}
+                  className='go-back'
+                  size={35}
+                  color={'#374756'}
+                
+                />
+              </button>
+              <h1 className="">{asset.name}</h1>
+            </div>
+            <Image src={asset.image} alt={asset.name} height={200} width={95} />
+            <div className="legend__description">
+              <p>{asset.description}</p>
+            </div>
+          </div>
+        </>
+      ))}
+    </>
+  );
+};
+
+export default Legend;
