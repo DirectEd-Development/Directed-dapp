dffa8e0ed01c93fc17b95557024ee43caf74dc9f
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-02 08:34:06 +0100

    feat: mint date timer

---
+++ components/Timer/Timer.tsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from 'react';
+
+type Props = {
+  targetDay: number;
+  targetMonth: number;
+  targetYear: number;
+};
+
+export const Timer: React.FC<Props> = ({ targetDay, targetMonth, targetYear }) => {
+  const targetDate = new Date(targetYear, targetMonth - 1, targetDay).toLocaleDateString('en-US', {
+    month: 'short',
+    day: 'numeric',
+    year: 'numeric',
+  });
+  const [timeRemaining, setTimeRemaining] = useState<number>(getTimeRemaining());
+
+  function getTimeRemaining(): number {
+    const currentTime = Date.now();
+    const targetTime = Date.UTC(targetYear, targetMonth - 1, targetDay, 20); 
+    const timeRemaining = targetTime - currentTime;
+    return Math.max(timeRemaining, 0);
+  }
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimeRemaining(getTimeRemaining());
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const days = Math.ceil(timeRemaining / (1000 * 60 * 60 * 24));
+
+  return (
+    <div>
+      <p>{targetDate} 8:00pm UTC</p>
+      <p>{days} days</p>
+    </div>
+  );
+};
+
+
+export default Timer;
