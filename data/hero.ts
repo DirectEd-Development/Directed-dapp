82ca8d737c4f61a54fbbd7c2d7984910b4fb67d1
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-19 12:05:30 +0100

    fix: nft payment links

--- data/hero.ts
+++ data/hero.ts
@@ -2,32 +2,32 @@ export const data = [
     {
       title: "Image 1",
       image: "/static/images/heros/1.png",
-      url: "https://example.com/image1"
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 2",
       image: "/static/images/heros/2.png",
-      url: "https://example.com/image2"
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 3",
       image: "/static/images/heros/3.png",
-      url: "https://example.com/image3"
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 4",
       image: "/static/images/heros/4.png",
-      url: "https://example.com/image4"
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 5",
       image: "/static/images/heros/5.png",
-      url: "https://example.com/image5"
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 6",
       image: "/static/images/heros/6.png",
-      url: "https://example.com/image6"
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
   ];
   
--- data/royal.ts
+++ data/royal.ts
@@ -1,33 +1,33 @@
 export const data = [
     {
       title: "Image 1",
-      image: "/static/images/heros/1.png",
-      url: "https://example.com/image1"
+      image: "/static/images/royals/1.png",
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 2",
-      image: "/static/images/heros/2.png",
-      url: "https://example.com/image2"
+      image: "/static/images/royals/2.png",
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 3",
-      image: "/static/images/heros/3.png",
-      url: "https://example.com/image3"
+      image: "/static/images/royals/3.png",
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 4",
-      image: "/static/images/heros/4.png",
-      url: "https://example.com/image4"
+      image: "/static/images/royals/4.png",
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 5",
-      image: "/static/images/heros/5.png",
-      url: "https://example.com/image5"
+      image: "/static/images/royals/5.png",
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
     {
       title: "Image 6",
-      image: "/static/images/heros/6.png",
-      url: "https://example.com/image6"
+      image: "/static/images/royals/6.png",
+      url: "https://pay.preprod.nmkr.io/?p=5edcc5ea394c4dea9ee0f91429534281&n=917403ccd8ca488583274cd670d8d162"
     },
   ];
   
