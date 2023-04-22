c55ed8b52e22f81a5e479a7495dbee213b3d7b9e
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-22 12:05:18 -0700

    fix: hero and royal button deactivated

--- components/TierCard/ManguCard.tsx
+++ components/TierCard/ManguCard.tsx
@@ -111,7 +111,7 @@ const ManguCard = ({ onClick, title, amount, image, school }: ManguCardProps) =>
 							pathname: '/nfts/royal',
 							query: school
 						}}>
-							<Button variant='primary'>Confirm Option</Button>
+							<Button variant='primary' disabled={isClose}>Confirm Option</Button>
 						</Link>
 						
 
@@ -140,7 +140,7 @@ const ManguCard = ({ onClick, title, amount, image, school }: ManguCardProps) =>
 							pathname: '/nfts/hero',
 							query: school
 						}}>
-							<Button variant='primary'>Confirm Option</Button>
+							<Button variant='primary' disabled={isClose}>Confirm Option</Button>
 						</Link>
 					</>
 				)}
