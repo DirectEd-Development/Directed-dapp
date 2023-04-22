8f5d4ed98c7ce8e0725adbd152bf8739041cd1ae
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-22 12:27:25 +0100

    fix: pass school to Optiontiers

--- lib/donorAmounts.ts
+++ lib/donorAmounts.ts
@@ -5,16 +5,19 @@ export const lionOptions: OptionTiers[] = [
 		amount: '2000',
 		title: 'Royal',
 		image: '/static/images/1.png',
+		school: '',
 	},
 	{
 		amount: '1000',
 		title: 'Hero',
 		image: '/static/images/2.png',
+		school: '',
 	},
 	{
 		amount: '40',
 		title: 'Warrior',
 		image: '/static/images/3.png',
+		school: '',
 	},
 ]
 
@@ -22,10 +25,12 @@ export const noLionOptions: OptionTiers[] = [
 	{
 		amount: '1000',
 		title: 'One Access Stipend',
+		school: '',
 	},
 	{
 		amount: 'custom',
 		title: 'Custom',
+		school: '',
 	},
 ]
 
