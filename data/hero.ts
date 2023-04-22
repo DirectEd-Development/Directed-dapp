90ff30e632f5ba14524db8ad1ed2e039b0e16d4e
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-22 13:05:01 +0100

    fix: hero data

--- data/hero.ts
+++ data/hero.ts
@@ -1,128 +1,56 @@
 export const data = [
   // Data for Kagumo High
   {
-    title: "Jumbo Darlido",
-    image: "/static/images/Directed Heros/Kagumo/Jumbo Darlido.png",
+    title: "Jonako Kariala",
+    image: "/static/images/Directed Royals/Kagumo Royals/Jonako Kariala.png",
     url: "",
     school: "kagumo",
   },
   {
-    title: "Sahara Mathi",
-    image: "/static/images/Directed Heros/Kagumo/Sahara Mathi.png",
-    url: "",
-    school: "kagumo",
-  },
-  {
-    title: "Shishu Kylwo",
-    image: "/static/images/Directed Heros/Kagumo/Shishu Kylwo.png",
-    url: "",
-    school: "kagumo",
-  },
-  {
-    title: "Tanya Naigu",
-    image: "/static/images/Directed Heros/Kagumo/Tanya Naigu.png",
-    url: "",
-    school: "kagumo",
-  },
-  {
-    title: "Zimba Laured",
-    image: "/static/images/Directed Heros/Kagumo/Zimba Laured.png",
+    title: "Mirca Teluji",
+    image: "/static/images/Directed Royals/Kagumo Royals/Mirca Teluji.png",
     url: "",
     school: "kagumo",
   },
 
   // Data for Mang'u High
   {
-    title: "Atilla Crimey",
-    image: "/static/images/Directed Heros/Mangu/Atilla Crimey.jpg",
+    title: "Bahati Yoxa",
+    image: "/static/images/Directed Royals/Mangu Royals/Bahati Yoxa.png",
     url: "",
     school: "Mangu",
   },
   {
-    title: "Kasandra Ribur",
-    image: "/static/images/Directed Heros/Mangu/Kasandra Ribur.png",
-    url: "",
-    school: "Mangu",
-  },
-  {
-    title: "Ligaro Gacaha",
-    image: "/static/images/Directed Heros/Mangu/Ligaro Gacaha.png",
-    url: "",
-    school: "Mangu",
-  },
-  {
-    title: "Mareo Razsam",
-    image: "/static/images/Directed Heros/Mangu/Mareo Razsam.png",
-    url: "",
-    school: "Mangu",
-  },
-  {
-    title: "Saritavak Chrupst",
-    image: "/static/images/Directed Heros/Mangu/Saritavak Chrupst.png",
+    title: "Yander Yoxa",
+    image: "/static/images/Directed Royals/Mangu Royals/Yander Yoxa.png",
     url: "",
     school: "Mangu",
   },
   
   // Data for Maryhill
   {
-    title: "Kaleen Adrus",
-    image: "/static/images/Directed Heros/Maryhill/Kaleen Adrus.jpg",
+    title: "Izbebraj Airode",
+    image: "/static/images/Directed Royals/Maryhill Royals/Izbebraj Airode.png",
     url: "",
     school: "Maryhill",
   },
   {
-    title: "Kola Alfmoe",
-    image: "/static/images/Directed Heros/Maryhill/Kola Alfmoe.jpg",
-    url: "",
-    school: "Maryhill",
-  },
-  {
-    title: "Locan Fabela",
-    image: "/static/images/Directed Heros/Maryhill/Locan Fabela.png",
-    url: "",
-    school: "Maryhill",
-  },
-  {
-    title: "Nemea Colecha",
-    image: "/static/images/Directed Heros/Maryhill/Nemea Colecha.png",
-    url: "",
-    school: "Maryhill",
-  },
-  {
-    title: "Theolas Rokele",
-    image: "/static/images/Directed Heros/Maryhill/Theolas Rokele.png",
+    title: "Yander Yoxa",
+    image: "/static/images/Directed Royals/Maryhill Royals/Mandar Kariala.png",
     url: "",
     school: "Maryhill",
   },
 
   // Data for NRCF
   {
-    title: "Ciara Yobezi",
-    image: "/static/images/Directed Heros/NRCF/Ciara Yobezi.png",
+    title: "Cehua Epkal",
+    image: "/static/images/Directed Royals/NRCF Royals/Cehua Epkal.png",
     url: "",
     school: "Ngong",
   },
   {
-    title: "Eqoso Muri",
-    image: "/static/images/Directed Heros/NRCF/Eqoso Muri.png",
-    url: "",
-    school: "Ngong",
-  },
-  {
-    title: "Kazu Stekin",
-    image: "/static/images/Directed Heros/NRCF/Kazu Stekin.png",
-    url: "",
-    school: "Ngong",
-  },
-  {
-    title: "Moja Nadka",
-    image: "/static/images/Directed Heros/NRCF/Moja Nadka.png",
-    url: "",
-    school: "Ngong",
-  },
-  {
-    title: "Moja Nadka",
-    image: "/static/images/Directed Heros/NRCF/Moja Nadka.png",
+    title: "Wada Teluji",
+    image: "/static/images/Directed Royals/NRCF Royals/Wada Teluji.png",
     url: "",
     school: "Ngong",
   },
