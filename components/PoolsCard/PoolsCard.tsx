b77f347c719cffa310ac0a9e909e2ea8051eed55
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-25 19:17:39 +0100

    fix: stakeAddress prop

--- components/PoolsCard/PoolsCard.tsx
+++ components/PoolsCard/PoolsCard.tsx
@@ -1,67 +1,71 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import Button from '../Button/Button'
-import ProgressBar from '../ProgressBar/ProgressBar'
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import Button from '../Button/Button';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
-const PoolsCard = ({
-	title,
-	image,
-	desc,
-}: {
-	title?: string
-	image?: string
-	desc?: string
-}) => {
-	return (
-		<div className='pools-card'>
-			<div className='pools-card__container'>
-				<Image
-					className='pools-card__image'
-					src={`${image}`}
-					alt='school image'
-					width='300'
-					height='350'
-				/>
-				<div className='pools-card__text-content'>
-					<div className='pools-card__details'>
-						<div>
-							<h3>{title}</h3>
-							<p>
-								Access Stipends for {desc} students from low income families.
-							</p>
-							<Link
-								href='https://www.notion.so/directed/Kagumo-High-School-bf13e9c623be4480a5a5c0aac3ebed18'
-								passHref
-								className='pools-card__link'
-							>
-								Learn More
-							</Link>
-						</div>
-					</div>
-					<div className='pools-card__funds'>
-						<div>
-							<h3>3</h3>
-							<p>Scholarships funded</p>
-						</div>
-						<hr />
-						<div>
-							<h3>23</h3>
-							<p>remaining Hero and Royal tier NFTs</p>
-						</div>
-					</div>
-					<div className='pools-card__progress'>
-						<ProgressBar />
-						<div className=''>
-							<Link href='/donate'>
-								<Button variant='primary'>donate now</Button>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
+interface PoolsCardProps {
+  title?: string;
+  image?: string;
+  desc?: string;
+  stakeAddress: string;
 }
 
-export default PoolsCard
+const PoolsCard = ({
+  title,
+  image,
+  desc,
+  stakeAddress,
+}: PoolsCardProps) => {
+  return (
+    <div className='pools-card'>
+      <div className='pools-card__container'>
+        <Image
+          className='pools-card__image'
+          src={`${image}`}
+          alt='school image'
+          width='300'
+          height='350'
+        />
+        <div className='pools-card__text-content'>
+          <div className='pools-card__details'>
+            <div>
+              <h3>{title}</h3>
+              <p>
+                Access Stipends for {desc} students from low income families.
+              </p>
+              <Link
+                href='https://www.notion.so/directed/Kagumo-High-School-bf13e9c623be4480a5a5c0aac3ebed18'
+                passHref
+                className='pools-card__link'
+              >
+                Learn More
+              </Link>
+            </div>
+          </div>
+          <div className='pools-card__funds'>
+            <div>
+              <h3>3</h3>
+              <p>Scholarships funded</p>
+            </div>
+            <hr />
+            <div>
+              <h3>23</h3>
+              <p>remaining Hero and Royal tier NFTs</p>
+            </div>
+          </div>
+          <div className='pools-card__progress'>
+            <ProgressBar stakeAddress={stakeAddress} />
+            <div className=''>
+              <Link href='/donate'>
+                <Button variant='primary'>donate now</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PoolsCard;
