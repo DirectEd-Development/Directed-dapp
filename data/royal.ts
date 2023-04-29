b699b217a5e5896d02cc1740413ad55b627d62ee
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-29 14:35:25 +0100

    fix: nmkr minting links added

--- data/royal.ts
+++ data/royal.ts
@@ -3,13 +3,13 @@ export const data = [
 	{
 		title: 'Jonako Kariala',
 		image: '/static/images/Directed-Royals/Kagumo-Royals/Jonako-Kariala.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=494bc66095c9465d88b288c71fd42913',
 		school: 'kagumo',
 	},
 	{
 		title: 'Mirca Teluji',
 		image: '/static/images/Directed-Royals/Kagumo-Royals/Mirca-Teluji.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=064b9926332b493585391e174d2bafd4',
 		school: 'kagumo',
 	},
 
@@ -17,13 +17,13 @@ export const data = [
 	{
 		title: 'Bahati Yoxa',
 		image: '/static/images/Directed-Royals/Mangu-Royals/Bahati-Yoxa.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=75a469cf3a8f4a29a7f546b638ebe15b',
 		school: 'Mangu',
 	},
 	{
 		title: 'Yander Yoxa',
 		image: '/static/images/Directed-Royals/Mangu-Royals/Yander-Yoxa.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=35f0f92b85b145209eef074f2e8271f1',
 		school: 'Mangu',
 	},
 
@@ -31,13 +31,13 @@ export const data = [
 	{
 		title: 'Izbebraj Airode',
 		image: '/static/images/Directed-Royals/Maryhill-Royals/Izbebraj-Airode.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=876f08619ee945e39ac7168a3cbb30f3',
 		school: 'Maryhill',
 	},
 	{
 		title: 'Mandar Kariala',
 		image: '/static/images/Directed-Royals/Maryhill-Royals/Mandar-Kariala.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=0dfb40d884d549c3816f9e3098644eab',
 		school: 'Maryhill',
 	},
 
@@ -45,13 +45,13 @@ export const data = [
 	{
 		title: 'Cehua Epkal',
 		image: '/static/images/Directed-Royals/NRCF-Royals/Cehua-Epkal.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=9af3caca510f4083ace39852d43446ff',
 		school: 'Ngong',
 	},
 	{
 		title: 'Wada Teluji',
 		image: '/static/images/Directed-Royals/NRCF-Royals/Wada-Teluji.png',
-		url: '',
+		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=0482880594b74aa2afc81d157da1540c',
 		school: 'Ngong',
 	},
 ]
