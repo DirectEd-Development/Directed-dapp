fbe73d77f1b7ed287001161fea5f9db6643e763b
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-04-29 15:06:09 +0100

    fix: image path

--- data/royal.ts
+++ data/royal.ts
@@ -2,13 +2,13 @@ export const data = [
 	// Data for Kagumo High
 	{
 		title: 'Jonako Kariala',
-		image: '/static/images/Directed-Royals/Kagumo-Royals/Jonako-Kariala.png',
+		image: '/static/images/Directed Royals/Kagumo-Royals/Jonako-Kariala.png',
 		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=494bc66095c9465d88b288c71fd42913',
 		school: 'kagumo',
 	},
 	{
 		title: 'Mirca Teluji',
-		image: '/static/images/Directed-Royals/Kagumo-Royals/Mirca-Teluji.png',
+		image: '/static/images/Directed Royals/Kagumo-Royals/Mirca-Teluji.png',
 		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=064b9926332b493585391e174d2bafd4',
 		school: 'kagumo',
 	},
@@ -16,13 +16,13 @@ export const data = [
 	// Data for Mang'u High
 	{
 		title: 'Bahati Yoxa',
-		image: '/static/images/Directed-Royals/Mangu-Royals/Bahati-Yoxa.png',
+		image: '/static/images/Directed Royals/Mangu-Royals/Bahati-Yoxa.png',
 		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=75a469cf3a8f4a29a7f546b638ebe15b',
 		school: 'Mangu',
 	},
 	{
 		title: 'Yander Yoxa',
-		image: '/static/images/Directed-Royals/Mangu-Royals/Yander-Yoxa.png',
+		image: '/static/images/Directed Royals/Mangu-Royals/Yander-Yoxa.png',
 		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=35f0f92b85b145209eef074f2e8271f1',
 		school: 'Mangu',
 	},
@@ -30,13 +30,13 @@ export const data = [
 	// Data for Maryhill
 	{
 		title: 'Izbebraj Airode',
-		image: '/static/images/Directed-Royals/Maryhill-Royals/Izbebraj-Airode.png',
+		image: '/static/images/Directed Royals/Maryhill-Royals/Izbebraj-Airode.png',
 		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=876f08619ee945e39ac7168a3cbb30f3',
 		school: 'Maryhill',
 	},
 	{
 		title: 'Mandar Kariala',
-		image: '/static/images/Directed-Royals/Maryhill-Royals/Mandar-Kariala.png',
+		image: '/static/images/Directed Royals/Maryhill-Royals/Mandar-Kariala.png',
 		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=0dfb40d884d549c3816f9e3098644eab',
 		school: 'Maryhill',
 	},
@@ -44,13 +44,13 @@ export const data = [
 	// Data for NRCF
 	{
 		title: 'Cehua Epkal',
-		image: '/static/images/Directed-Royals/NRCF-Royals/Cehua-Epkal.png',
+		image: '/static/images/Directed Royals/NRCF-Royals/Cehua-Epkal.png',
 		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=9af3caca510f4083ace39852d43446ff',
 		school: 'Ngong',
 	},
 	{
 		title: 'Wada Teluji',
-		image: '/static/images/Directed-Royals/NRCF-Royals/Wada-Teluji.png',
+		image: '/static/images/Directed Royals/NRCF-Royals/Wada-Teluji.png',
 		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=0482880594b74aa2afc81d157da1540c',
 		school: 'Ngong',
 	},
