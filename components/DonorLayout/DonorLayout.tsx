d4b87802376a571785ea956f0d489e4810cc4020
jonniek01 <jontejonhn@gmail.com>
2023-05-09 07:54:46 +0300

    imported Layout independently to each page

---
+++ components/DonorLayout/DonorLayout.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+type LayoutProps = {
+	children: React.ReactNode
+}
+
+const DonorLayout = ({ children }: LayoutProps) => {
+	return (
+        <Layout>
+            <div className="donor-nav">
+                <h2>Donor Nav</h2>
+                <ul>
+                    <li><a href="/donors-portal/nft-gallery">NFT Gallery</a></li>
+                    <li><a href="/donors-portal/scholar-progress">Progress</a></li>
+                </ul>
+            </div>
+            <>{children}</>
+        </Layout>
+		
+	)
+}
+
+export default DonorLayout
---
+++ pages/donors-portal/scholars-progress/index.tsx
@@ -0,0 +1,24 @@
+import { StudentMilestone } from '../../../components'
+
+type Props = {}
+
+const ScholarProgress = (props: Props) => {
+	return (
+		<main className='scholar-progress'>
+			<h2>Scholars' Progress</h2>
+			<section>
+				<h3>Progress Track for St. Peters High School</h3>
+				<div className='scholar-progress__student-list'>
+					<div className='scholar-progress__student-list--head'>
+						<p>Name</p>
+						<p>Milestone Progress</p>
+						<p>Contact</p>
+					</div>
+					<StudentMilestone />
+				</div>
+			</section>
+		</main>
+	)
+}
+
+export default ScholarProgress
