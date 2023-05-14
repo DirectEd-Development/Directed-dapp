eafd7f4091f6dddef7b241ec6b45bfcd02b99b53
Brave-source <eebiyenrin@gmail.com>
2023-05-15 00:26:51 +0100

    fix: text correction in gallery

--- pages/donors-portal/scholars-progress/index.tsx
+++ pages/donors-portal/scholars-progress/index.tsx
@@ -7,6 +7,17 @@ const ScholarProgress = (props: Props) => {
     <DonorLayout>
       <main className='scholar-progress'>
         <div className='work-in-progress'>
+          {/* <section>
+				<h3>Progress Track for St. Peters High School</h3>
+				<div className='scholar-progress__student-list'>
+					<div className='scholar-progress__student-list--head'>
+						<p>Name</p>
+						<p>Milestone Progress</p>
+						<p>Contact</p>
+					</div>
+					<StudentMilestone />
+				</div>
+			</section> */}
           <h2>Page Under Development</h2>
           <p>This page is currently being worked on and is not yet available.</p>
           <p>Please check back later for updates.</p>
