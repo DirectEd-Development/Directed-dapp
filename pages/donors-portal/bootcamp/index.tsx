5e964e75c239e2a1dfc29b110ef164ed92bb3b52
Brave-source <eebiyenrin@gmail.com>
2023-05-15 00:19:42 +0100

    fix: text fixes on donors portal

--- pages/donors-portal/bootcamp/index.tsx
+++ pages/donors-portal/bootcamp/index.tsx
@@ -11,7 +11,7 @@ const Bootcamp = (props: Props) => {
 				<h3>Access the DirectEd Bootcamp Course</h3>
 				<div className='bootcamp-section__info'>
 					<div className='bootcamp-section__details'>
-						<p>To access the DirectEd Bootcamp content, send an email to <a href="mailto:edu@directed.dev">edu@directed.dev</a> to be added to all study materials, then follow the link <a href="mailto:edu@directed.dev">Here</a></p>
+						<p>To access the DirectEd Bootcamp content, send an email to <a href="mailto:edu@directed.dev">edu@directed.dev</a> to be added to all study materials, then follow the link <a href="https://directed.notion.site/DirectEd-Bootcamp-2023-be6da237c8ce423588620f78e69292d1">Here</a></p>
 					</div>
 				</div>
 			</section>
--- pages/donors-portal/events/index.tsx
+++ pages/donors-portal/events/index.tsx
@@ -4,7 +4,7 @@ import moment from 'moment-timezone';
 type Props = {}
 
 const Events = (props: Props) => {
-  const eventDate = moment.tz('2023-05-15 06:00:00', 'Africa/Lagos');
+  const eventDate = moment.tz('2023-05-15 06:00:00', 'UTC');
 
   const formatDate = (date: moment.Moment) => {
     return date.format('ddd MMM D, YYYY h:mm A z');
@@ -19,7 +19,7 @@ const Events = (props: Props) => {
             <div className='event-section__event-info'>
               <h3>DirectEd Scholars 2023 Opening Ceremony</h3>
               <p>You can join the opening ceremony via this link: <a href="https://lu-se.zoom.us/j/66085258419">Zoom</a></p>
-              <p>Date: {formatDate(eventDate)} | Sun May 14, 2023 11:00 PM BST | Mon May 15, 2023 7:00 AM CEST</p>
+              <p>Date: {formatDate(eventDate)} | Sun May 14, 2023 11:00 PM PST</p>
             </div>
           </div>
         </section>
