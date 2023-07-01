15d5f46229202582e117310a6c5792d69c0d5a2b
Brave-source <eebiyenrin@gmail.com>
2023-07-01 20:32:20 +0100

    fix: indent

--- pages/directed-donate/index.tsx
+++ pages/directed-donate/index.tsx
@@ -12,7 +12,7 @@ import { ModalHandler} from '../../components/Modal/Modal'
 import {Layout} from '../../components'
 
 const donationAddress =
-		'addr1x8kmhudzykxz3d87tr7jn27n34uulzed2asqvdpauhvrl78dh0c6yfvv9z60uk8a9x4a8rtee79j64mqqc6rmewc8luq4hm7e2'
+	'addr1x8kmhudzykxz3d87tr7jn27n34uulzed2asqvdpauhvrl78dh0c6yfvv9z60uk8a9x4a8rtee79j64mqqc6rmewc8luq4hm7e2'
 
 const amounts = ['1000', '300', '100']
 
