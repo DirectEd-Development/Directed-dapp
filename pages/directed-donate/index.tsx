3a84bd51e1827715456415aab6135a0852fb1204
jonniek01 <jontejonhn@gmail.com>
2023-09-04 09:58:06 +0300

    email form set up

--- pages/directed-donate/index.tsx
+++ pages/directed-donate/index.tsx
@@ -28,6 +28,15 @@ const DirectedDonate: NextPage<DirectedDonateProps> = () => {
   const successModalRef = useRef<ModalHandler>(null);
   const errorModalRef = useRef<ModalHandler>(null);
   const feedbackModalRef = useRef<ModalHandler>(null);
+  const emailModalRef = useRef<ModalHandler>(null);
+
+  //radio buttons
+  const [selectedValue, setSelectedValue] = useState('');
+
+  const handleButtonClick = (value: string) => {
+    setSelectedValue(value);
+  };
+
 
   const router = useRouter();
   const { wallet, connected } = useWallet();
@@ -91,6 +100,7 @@ const DirectedDonate: NextPage<DirectedDonateProps> = () => {
     setProcessing(false);
   };
 
+emailModalRef.current?.openModal();
   return (
     <Layout>
       <Meta title='Donate' description='Donate to student.' />
@@ -226,6 +236,68 @@ const DirectedDonate: NextPage<DirectedDonateProps> = () => {
           </div>
         </div>
       </Modal>
+
+      <Modal ref={emailModalRef}>
+        <div className='email__modal'>
+          <h2>Welcome to the DirectEd Lion Family!</h2>
+          <h2>🦁</h2>
+          <p>Thank you for your generous donation. We would for you to see what difference you are making. </p>
+          <p>To ensure we can keep you updated, Please provide your email below</p>
+          <div>
+            <input type="text" placeholder="Name/ Nickname" />
+            <input type="text" placeholder="Your email" />
+          </div>
+          <div>
+            <span>
+            <input type="checkbox" value='receipt' />
+            <label>Send me a receipt</label>
+            </span>
+            <span>
+            <input type="checkbox" value='news' />
+            <label>Send me newsletter</label>
+            </span>
+
+          </div>
+          <div>
+            <h3>Select donor tiar</h3>
+            <div>
+            <Button
+        variant={selectedValue === 'warrior' ? 'primary' : 'light'}
+        onClick={() => handleButtonClick('warrior')}
+      >
+        Warrior
+      </Button>
+      <Button
+        variant={selectedValue === 'hero' ? 'primary' : 'light'}
+        onClick={() => handleButtonClick('hero')}
+      >
+        Hero
+      </Button>
+      <Button
+        variant={selectedValue === 'royal' ? 'primary' : 'light'}
+        onClick={() => handleButtonClick('royal')}
+      >
+        Royal
+      </Button>
+            </div>
+
+
+          </div>
+          <div>
+              <Button
+              variant='primary'
+              >Submit</Button>
+            </div>
+          <p>
+          By entering your email, you agree to be on our donor's mailing list. We will send no more than 10 emails per year. You can unsubscribe at any point.
+          </p>
+          </div>
+      </Modal>
+
+
+
+          
+
 
       <Modal ref={feedbackModalRef}>
         <div className='feedback__modal-content'>
