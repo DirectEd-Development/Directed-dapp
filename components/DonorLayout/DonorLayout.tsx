47819402ce8d56c378aefe70746b84afc7c133f9
itsrandymungai <randymungai20@gmail.com>
2023-09-15 09:46:41 +0300

    added donor's portal layout icons

--- components/DonorLayout/DonorLayout.tsx
+++ components/DonorLayout/DonorLayout.tsx
@@ -3,6 +3,15 @@ import Link from 'next/link'
 import Layout from '../Layout/Layout'
 import {AiOutlineRight} from 'react-icons/ai'
 import { usePathname } from 'next/navigation'
+import { 
+	NFTGalleryIcon,
+	ScholarProgressIcon,
+	EventsIcon,
+	BootcampIcon,
+	MoneyInIcon,
+	MoneyOutIcon
+
+} from '../../icons'
 
 
 type LayoutProps = {
@@ -56,10 +65,20 @@ const DonorLayout = ({ children }: LayoutProps) => {
 									color: path === link.link ?  '#fff':'#000',
 								}
 							}
-							 href={link.link} key={index}>
+							 href={link.link} key={index} className='link-container'>
+								
+								<div className='link-container'>
+									{link.name === 'NFT Gallery' && <NFTGalleryIcon />}
+									{link.name === 'Scholars\' Progress' && <ScholarProgressIcon/>}
+									{link.name === 'Events' && <EventsIcon/>}
+									{link.name === 'DirectEd Bootcamp' && <BootcampIcon />}
+									{link.name === 'Money In' && <MoneyInIcon />}
+									{link.name === 'Money Out' && <MoneyOutIcon />}
+								
 								<p>
 									{link.name}
 								</p>
+								</div>
 								
 									<AiOutlineRight
 										className='donors__link-icon'
