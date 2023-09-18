1134da047a3a7d0da9b4b58efd017713ccdef3cf
Brave-Source <Brave-Source>
2023-09-18 09:21:53 +0100

    fix: nav indents

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -13,12 +13,11 @@ const NavLinks: LinksType[] = [
 	{ title: 'Why DirectEd?', link: '/' },
 	{ title: 'Scholarship Crowdfunding', link: '/scholarship-pool' },
 	{ title: "Donor's Portal", link: '/donors-portal/nft-gallery' },
-	{ title: 'About Us', link: 'https://www.notion.so/directed/About-Us-1be24f1a03ca4c1298dfb9ed20007d2a'}
+	{ title: 'About Us', link: 'https://www.notion.so/directed/About-Us-1be24f1a03ca4c1298dfb9ed20007d2a' }
 ]
 
 const Navbar = () => {
 	const [open, setOpen] = React.useState<boolean>(false)
-
 	const router = useRouter()
 
 	return (
@@ -45,9 +44,8 @@ const Navbar = () => {
 						{NavLinks.map((link: LinksType, index: number) => (
 							<Link href={link.link} passHref key={index}>
 								<li
-									className={` ${
-										router.pathname === link.link ? 'active' : ''
-									}`}
+									className={` ${router.pathname === link.link ? 'active' : ''
+										}`}
 								>
 									{link.title}
 								</li>
