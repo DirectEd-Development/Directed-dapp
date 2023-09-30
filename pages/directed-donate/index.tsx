f141972ad2601da9a93e0445eddca17b975ad893
ndigirigijohn <jontejonhn@gmail.com>
2023-09-30 13:31:58 +0300

    crypto donations progress

--- pages/directed-donate/index.tsx
+++ pages/directed-donate/index.tsx
@@ -1,10 +1,26 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { NextPage } from 'next';
 
-import { Layout, Button, ConnectWallet } from '../../components';
+import { useRouter } from 'next/router';
+
+import { Layout, Button, ConnectWallet, Modal } from '../../components';
 
 import {AiFillCreditCard} from 'react-icons/ai'
 import {RiCoinsLine} from 'react-icons/ri'
+import { useWallet } from '@meshsdk/react';
+import { Transaction } from '@meshsdk/core';
+
+
+import Survey from '../../components/Survey/Survey';
+
+
+import { ModalHandler } from '../../components/Modal/Modal';
+
+const donationAddress =
+  'addr_test1qzzanma8z97ltgkg4nup2ta8fyzrgnsz9ur28undfzczsrjy9axchrqjt0rtk22rxr2zp5rquku4a2k4c4cw0duwug2s62nzxf';
+
+
+
 
 interface DirectedDonateProps {}
 
@@ -15,6 +31,74 @@ const DirectedDonate: NextPage<DirectedDonateProps> = () => {
     const [currency, setCurrency] = useState('');
     const [paymentMethod, setPaymentMethod] = useState('');
     const [value, setValue] = useState(1250)
+
+    /*Crypto processing states*/
+        const [confirmModalVisible, setConfirmModalVisible] = useState<boolean>(false);
+
+        const [processing, setProcessing] = useState<boolean>(false);
+        const [errorMessage, setErrorMessage] = useState<string | null>(null);
+        const [successfulTxHash, setSuccessfulTxHash] = useState<string | null>(null);
+        const [triggerConfirm, setTriggerConfirm] = useState<boolean>(false);
+
+        const confirmModalRef = useRef<ModalHandler>(null);
+        const successModalRef = useRef<ModalHandler>(null);
+        const errorModalRef = useRef<ModalHandler>(null);
+        const feedbackModalRef = useRef<ModalHandler>(null);
+
+        const { wallet, connected } = useWallet();
+      useEffect(() => {
+        if(triggerConfirm&& wallet ){
+            confirmModalRef.current?.openModal();
+        }
+    },[wallet, triggerConfirm])
+
+        const handleDonate = async (sendAmount: string) => {
+            setProcessing(true);
+            const convertLovelace = parseInt(sendAmount) * 1000000;
+            const sendLovelace = convertLovelace.toString();
+        
+            if (connected) {
+              try {
+                const network = await wallet.getNetworkId();
+        
+                if (network === 1) {
+                  setErrorMessage('This dapp only works on Cardano Testnet.');
+                  errorModalRef.current?.openModal();
+                  setConfirmModalVisible(false);
+                  setProcessing(false);
+                } else {
+                  const tx = new Transaction({ initiator: wallet }).sendLovelace(donationAddress, sendLovelace);
+                  const unsignedTx = await tx.build();
+                  const signedTx = await wallet.signTx(unsignedTx);
+                  const txHash = await wallet.submitTx(signedTx);
+        
+                  setSuccessfulTxHash(txHash);
+                  successModalRef.current?.openModal();
+                  setAmount(sendAmount);
+                  setConfirmModalVisible(false);
+                  setProcessing(false);
+                }
+              } catch (error: any) {
+                if (error.info) {
+                  setErrorMessage(error.info);
+                } else if (error.message.includes('Insufficient')) {
+                  setErrorMessage(`Insufficient funds in your wallet to send ${amount} ADA. Please top up your wallet and try again.`);
+                } else {
+                  setErrorMessage('Something went wrong. Please try again.');
+                }
+                errorModalRef.current?.openModal();
+                setProcessing(false);
+                setConfirmModalVisible(false);
+              }
+            } else {
+              setErrorMessage('Please connect a wallet.');
+              errorModalRef.current?.openModal();
+              setProcessing(false);
+              setConfirmModalVisible(false);
+            }
+          };
+        
+      
     
 
     const connectWallet = () =>{
@@ -22,6 +106,8 @@ const DirectedDonate: NextPage<DirectedDonateProps> = () => {
             <ConnectWallet
             />
         )
+        setTriggerConfirm(true)
+
     }
 
     const handleDonation = () => {
@@ -315,7 +401,113 @@ const DirectedDonate: NextPage<DirectedDonateProps> = () => {
             </div>
             {VARYING}
         </div>
-        </Layout>
+              {/* Modals */}
+      <Modal ref={confirmModalRef}>
+        <div className='confirm__modal-content'>
+          <div className='confirm__modal-header'>
+            <h4>Confirm Donation</h4>
+          </div>
+          <div className='confirm__modal-body'>
+            <span className='amount'>
+              Amount: 
+            </span>
+            <span className="ada">
+              ₳{amount}
+            </span>
+          </div>
+          <div className='confirm__modal-footer'>
+            <Button
+              disabled={processing}
+              variant='primary' 
+              onClick={() => handleDonate(amount)}
+            >
+              {processing ? 'Processing...' : 'Confirm'}
+            </Button>
+            <Button 
+              variant='primary' 
+              onClick={() => {
+                setConfirmModalVisible(false);
+                setProcessing(false);
+                confirmModalRef.current?.closeModal();
+              }}
+            >
+              Cancel
+            </Button>
+          </div>
+        </div>
+      </Modal>
+
+      <Modal ref={successModalRef}>
+        <div className='success__modal-content'>
+          <div className='success__modal-header'>
+            Thank you for your support! Your <span className='bold-text hash'>₳{amount}</span> donation was well received. Transaction Hash: <span className='bold-text'>{successfulTxHash}</span>. This will go far in supporting many dreams.
+          </div>
+          <div className='success__modal-footer'>
+            <Button
+              variant='primary' 
+              onClick={() => {
+                setConfirmModalVisible(false);
+                successModalRef.current?.closeModal();
+                confirmModalRef.current?.closeModal();
+              }}
+            >
+              CLOSE
+            </Button>
+          </div>
+        </div>
+      </Modal>
+
+      <Modal ref={errorModalRef}>
+        <div className='error__modal-content'>
+          <div className="error__modal-body">
+            <p>
+              {errorMessage}
+            </p>
+          </div>
+          <div className="error__modal-footer">
+            <Button
+              variant='primary'
+              onClick={() => {
+                errorModalRef.current?.closeModal();
+              }}
+            >
+              OK
+            </Button>
+          </div>
+        </div>
+      </Modal>
+
+      <Modal ref={feedbackModalRef}>
+        <div className='feedback__modal-content'>
+          <button
+            className='close-modal-button'
+            onClick={() => {
+              setConfirmModalVisible(false);
+              feedbackModalRef.current?.closeModal();
+            }}
+          >
+            X
+          </button>
+          <div className="feedback__modal-header">
+            <h4>Help us improve the donation process!</h4>
+          </div>
+          <div className="feedback__modal-body">
+            <Survey />
+          </div>
+          <div className="feedback__modal-footer">
+            <Button
+              variant='primary'
+              onClick={() => {
+                feedbackModalRef.current?.closeModal();
+              }}
+            >
+              CLOSE
+            </Button>
+          </div>
+        </div>
+      </Modal>
+    </Layout>
+
 
     )
 }
