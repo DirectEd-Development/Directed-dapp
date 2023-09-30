f367eb9e2395351bf90cc62442a2f0cf119e2796
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-09-30 20:28:15 +0100

    fix: remove yali logo

--- pages/scholarship-pool/index.tsx
+++ pages/scholarship-pool/index.tsx
@@ -130,12 +130,6 @@ const ScholarshipPool: NextPage = () => {
 							height={50}
 						/>
 						<Image
-							src='/static/images/yali.png'
-							alt='yali'
-							width={65}
-							height={65}
-						/>
-						<Image
 							src='/static/images/tally.svg'
 							alt='tally'
 							width={100}
