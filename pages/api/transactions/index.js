da6af1005862dbe92624dc2725461cc8994a4807
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-10-02 02:15:16 +0100

    fix: fix mixed content issue

--- pages/api/transactions/index.js
+++ pages/api/transactions/index.js
@@ -1,36 +1,41 @@
 import dbConnect from "../../../utils/mongo";
 import Transaction from "../../../utils/Transaction";
-import {updateNftMetadata, getAllNfts} from "../updateMetadata";
+import { updateNftMetadata, getAllNfts } from "../updateMetadata";
 
-export default async function handler (req, res) {
-    const {method} = req;
+export default async function handler(req, res) {
+  const { method } = req;
 
-    // await dbConnect();
-    // 'https://studio-api.preprod.nmkr.io/v2/UpdateMetadata/5edcc5ea-394c-4dea-9ee0-f91429534281/e569a247-5158-43f7-ba37-ada6cbe7d2aa'
+  // Uncomment the following line if you have a database to connect to
+  // await dbConnect();
 
-    if(method === "POST") {
-       try {
-        const result = await getAllNfts();
-        res.status(200).json(result);
-       }catch(err) {
-        console.log(err);
-       }
+  if (method === "POST") {
+    try {
+      const result = await getAllNfts();
+      res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
     }
+  }
 
-    if(method === "GET") {
-    //    try {
-    //     const transaction = await Transaction.create(req.body);
-    //     res.status(200).json(transaction);
-    //    }catch(err) {
-    //     res.status(500).json(err);
-    //    }
+  if (method === "GET") {
+    // Uncomment the following lines if you want to handle GET requests
+    // try {
+    //   const transaction = await Transaction.create(req.body);
+    //   res.status(200).json(transaction);
+    // } catch (err) {
+    //   console.error(err);
+    //   res.status(500).json({ error: "Internal Server Error" });
+    // }
+  }
+
+  if (method === "PUT") {
+    try {
+      const result = await updateNftMetadata(req.body);
+      res.status(200).json(result);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "Internal Server Error" });
     }
-    if(method === "PUT") {
-        try{
-           const result = await updateNftMetadata(req.body);
-           res.status(200).json(result);
-        }catch(err) {
-            console.log(err);
-        }
-    }
+  }
 }
