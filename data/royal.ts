8ddec5bff6366fc493853e99af640c5b30664c59
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-10-04 15:59:30 +0100

    fix: royal/hero nfts

--- data/royal.ts
+++ data/royal.ts
@@ -16,28 +16,28 @@ export const data = [
 	// Data for Boys Pool
 	{
 		title: 'Bahati Yoxa',
-		image: '/static/images/Directed Royals/Mangu-Royals/Bahati-Yoxa.png',
-		url: '',
+		image: '/static/images/Directed Royals/boyspool/BahatiYoxa.png',
+		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=75a469cf3a8f4a29a7f546b638ebe15b',
 		school: 'boyspool',
 	},
 	{
-		title: 'Yander Yoxa',
-		image: '/static/images/Directed Royals/Mangu-Royals/Yander-Yoxa.png',
-		url: '',
+		title: 'Mirca Teluji',
+		image: '/static/images/Directed Royals/boyspool/MircaTeluji.png',
+		url: 'https://pay.nmkr.io/?p=ed7a0468943242918de5cfba4fe8766d&n=064b9926332b493585391e174d2bafd4',
 		school: 'boyspool',
 	},
 
 	// Data for Girls Pool
 	{
-		title: 'Izbebraj Airode',
-		image: '/static/images/Directed Royals/Maryhill-Royals/Izbebraj-Airode.png',
-		url: '',
+		title: 'Cehua Epkal',
+		image: '/static/images/Directed Royals/girlspool/CehuaEpkal.png',
+		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=9af3caca510f4083ace39852d43446ff',
 		school: 'girlspool',
 	},
 	{
-		title: 'Mandar Kariala',
-		image: '/static/images/Directed Royals/Maryhill-Royals/Mandar-Kariala.png',
-		url: '',
+		title: 'Cehua Epkal2',
+		image: '/static/images/Directed Royals/girlspool/CehuaEpkal2.png',
+		url: 'https://pay.nmkr.io/?p=1c73676dd6d04e8bb5e08d1eedd404ad&n=4fa8091981124dca9b4f0797aa8cef0b',
 		school: 'girlspool',
 	},
 
