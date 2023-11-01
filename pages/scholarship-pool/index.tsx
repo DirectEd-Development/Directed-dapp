9caeb64d8637bc942223ca1d9b0c769437c48324
raymond <raymondezra276@gmail.com>
2023-11-01 12:55:06 +0300

    update

--- pages/scholarship-pool/index.tsx
+++ pages/scholarship-pool/index.tsx
@@ -87,7 +87,7 @@ const ScholarshipPool: NextPage = () => {
 						<ScholarsCard
 							donated={wallet2.length}
 							funded='0'
-							fundsLeft='10'
+							fundsLeft='14'
 							schoolName='General Pool'
 							amount='7000$'
 							schoolAlias='DirectEd General Pool'
@@ -102,7 +102,7 @@ const ScholarshipPool: NextPage = () => {
 							funded='0'
 							fundsLeft='10'
 							schoolName="Boys Pool"
-							amount='5000$'
+							amount='7000$'
 							schoolAlias="DirecEd Boys Pool"
 							infoLink='https://directed.notion.site/DirectEd-s-EmpowerHim-Boys-only-funding-pool-eab27b1341b04da0b0e330e0e999be07?pvs=4'
 							image='/static/images/boyspool.jpg'
@@ -113,9 +113,9 @@ const ScholarshipPool: NextPage = () => {
 						<ScholarsCard
 							donated={wallet1.length}
 							funded='0'
-							fundsLeft='10'
+							fundsLeft='4'
 							schoolName="Girls Pool"
-							amount='2000$'
+							amount='7000$'
 							schoolAlias="DirectEd Girls Pool"
 							infoLink='https://directed.notion.site/DirectEd-s-EmpowerHer-Girls-only-funding-pool-fa956750d38d4e98b58655a15f2e2c9e?pvs=4'
 							image='/static/images/girlspool.jpg'
