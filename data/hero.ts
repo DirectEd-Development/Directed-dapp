e2f33031ffc92528dd60d4c79ff8d1a1f5ffada8
Ebiyenrin Edmund <eebiyenrin@gmail.com>
2023-11-13 06:46:54 +0100

    fix: nft path

--- data/hero.ts
+++ data/hero.ts
@@ -1,87 +1,86 @@
 export const data = [
-	// Data for DirectEd Scholars
-	{
-		title: 'Kasandra Ribur',
-		image: '/static/images/Directed Heros/directedscholars/KasandraRibur.png',
-		url: 'https://pay.nmkr.io/?p=1ce018420cd247ac949c2427d34d6db3&n=0336d15025e04eba8b80a98cdd9d5015',
-		school: 'generalpool',
-	},
-	{
-		title: 'Nemea Colecha',
-		image: '/static/images/Directed Heros/directedscholars/NemeaColecha.png',
-		url: 'https://pay.nmkr.io/?p=1ce018420cd247ac949c2427d34d6db3&n=da0dc5713b1f4f37b1c70674bfa36396',
-		school: 'generalpool',
-	},
-	{
-		title: 'Kaleen Adrus',
-		image: '/static/images/Directed Heros/directedscholars/KaleenAdrus.jpeg',
-		url: 'https://pay.nmkr.io/?p=1ce018420cd247ac949c2427d34d6db3&n=bb908108a13344b79255d0680ca39e32',
-		school: 'generalpool',
-	},
-	
+  // Data for DirectEd Scholars
+  {
+    title: "Kasandra Ribur",
+    image: "/static/images/Directed Heros/directedscholars/KasandraRibur.png",
+    url: "https://pay.nmkr.io/?p=1ce018420cd247ac949c2427d34d6db3&n=0336d15025e04eba8b80a98cdd9d5015",
+    school: "generalpool",
+  },
+  {
+    title: "Nemea Colecha",
+    image: "/static/images/Directed Heros/directedscholars/NemeaColecha.png",
+    url: "https://pay.nmkr.io/?p=1ce018420cd247ac949c2427d34d6db3&n=da0dc5713b1f4f37b1c70674bfa36396",
+    school: "generalpool",
+  },
+  {
+    title: "Kaleen Adrus",
+    image: "/static/images/Directed Heros/directedscholars/KaleenAdrus.jpeg",
+    url: "https://pay.nmkr.io/?p=1ce018420cd247ac949c2427d34d6db3&n=bb908108a13344b79255d0680ca39e32",
+    school: "generalpool",
+  },
 
-	// Data for Boys Pool
-	{
-		title: 'Moja Nadka',
-		image: '/static/images/Directed Heros/boyspool/MojaNadka.png',
-		url: '',
-		school: 'boyspool',
-	},
-	{
-		title: 'Eqoso Muri',
-		image: '/static/images/Directed Heros/boyspool/EqosoMuri.png',
-		url: 'https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=d732a15fe7a345d297aae81e965b53de',
-		school: 'boyspool',
-	},
-	{
-		title: 'Kola Alfmoe',
-		image: '/static/images/Directed Heros/boyspool/KolaAlfmoe.jpeg',
-		url: 'https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=60a5a86750484ab6949562428cc9d4b8',
-		school: 'boyspool',
-	},
-	{
-		title: 'Locan Fabela',
-		image: '/static/images/Directed Heros/boyspool/LocanFabela.png',
-		url: 'https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=91952ec08b0940b2b56b14e6a6411105',
-		school: 'boyspool',
-	},
-	{
-		title: 'Theolas Rokele',
-		image: '/static/images/Directed Heros/boyspool/TheolasRokele.png',
-		url: 'https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=2bcf8d5bcee74d9a8c33f1042c370692',
-		school: 'boyspool',
-	},
+  // Data for Boys Pool
+  {
+    title: "Moja Nadka",
+    image: "/static/images/Directed Heros/boyspool/MojaNadka.png",
+    url: "",
+    school: "boyspool",
+  },
+  {
+    title: "Eqoso Muri",
+    image: "/static/images/Directed Heros/boyspool/EqosoMuri.png",
+    url: "https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=d732a15fe7a345d297aae81e965b53de",
+    school: "boyspool",
+  },
+  {
+    title: "Kola Alfmoe",
+    image: "/static/images/Directed Heros/boyspool/KolaAlfmoe.jpeg",
+    url: "https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=60a5a86750484ab6949562428cc9d4b8",
+    school: "boyspool",
+  },
+  {
+    title: "Locan Fabela",
+    image: "/static/images/Directed Heros/boyspool/LocanFabela.png",
+    url: "https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=91952ec08b0940b2b56b14e6a6411105",
+    school: "boyspool",
+  },
+  {
+    title: "Theolas Rokele",
+    image: "/static/images/Directed Heros/boyspool/TheolasRokele.png",
+    url: "https://pay.nmkr.io/?p=ab9ebe10673b43cba568b2f54438dc48&n=2bcf8d5bcee74d9a8c33f1042c370692",
+    school: "boyspool",
+  },
 
-	// Data for Girls Pool
+  // Data for Girls Pool
 
-	{
-		title: 'Mareo Razsam',
-		image: '/static/images/Directed Heros/girlspool/MareoRazsam.png',
-		url: 'https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=4394212e28f64d3b9d25abb6740c4713',
-		school: 'girlspool',
-	},
-	{
-		title: 'Saritava Chrups',
-		image: '/static/images/Directed Heros/girlspool/SaritavaChrups.png',
-		url: 'https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=e0bdbe3a98f0470f804035d6adc1f5a1',
-		school: 'girlspool',
-	},
-	{
-		title: 'Atilla Crimey',
-		image: '/static/images/Directed Heros/girlspool/AtillaCrimey.jpeg',
-		url: 'https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=edaa5c98ffb541caaaa2069787db29ad',
-		school: 'girlspool',
-	},
-	{
-		title: 'Kasandra Ribur',
-		image: '/static/images/Directed Heros/girlspool/KasandraRibur.png',
-		url: 'https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=99aa4efc6b3247cba0105bc411814ac7',
-		school: 'girlspool',
-	},
-	{
-		title: 'Tanya Naigu',
-		image: '/static/images/Directed Heros/girlspool/TanyaNaigu.png',
-		url: 'https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=fac7c0827bb1413ab31413f1e9c22617',
-		school: 'girlspool',
-	},
-]
+  {
+    title: "Mareo Razsam",
+    image: "/static/images/Directed Heros/girlspool/MareoRazsam.png",
+    url: "https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=4394212e28f64d3b9d25abb6740c4713",
+    school: "girlspool",
+  },
+  {
+    title: "Saritava Chrups",
+    image: "/static/images/Directed Heros/girlspool/SaritavaChrups.png",
+    url: "https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=e0bdbe3a98f0470f804035d6adc1f5a1",
+    school: "girlspool",
+  },
+  {
+    title: "Atilla Crimey",
+    image: "/static/images/Directed Heros/girlspool/AtillaCrimey.jpeg",
+    url: "https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=edaa5c98ffb541caaaa2069787db29ad",
+    school: "girlspool",
+  },
+  {
+    title: "Kasandra Ribur",
+    image: "/static/images/Directed Heros/girlspool/KasandraRibur.png",
+    url: "https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=99aa4efc6b3247cba0105bc411814ac7",
+    school: "girlspool",
+  },
+  {
+    title: "Tanya Naigu",
+    image: "/static/images/Directed Heros/girlspool/TanyaNaigu.png",
+    url: "https://pay.nmkr.io/?p=528af4d0868a4442b5e2caedd3979813&n=fac7c0827bb1413ab31413f1e9c22617",
+    school: "girlspool",
+  },
+];
